8cc817e8453c2e5df2213f54b11cf5ca3953570d
Olli-Antti Kivilahti <kivilahtio@gmail.com>
2017-04-26 13:41:42 +0300

    Crontab for exporting daily worktimes

--- INSTALL.sh
+++ INSTALL.sh
@@ -3,3 +3,4 @@ apt-get install cpanminus
 cpanm Modern::Perl DateTime::Format::MySQL DateTime::Format::Duration Text::CSV ODF::lpOD Params::Validate Try::Tiny Test::More Test::MockModule DBI Log::Log4perl DBI DBD::mysql
 
 
+cp scripts/crontab /etc/cron.d/rms
---
+++ scripts/exportToKatkola.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+##
+## Export worktimes for given users and push them to Kätkölä
+##
+
+function putti {
+
+    username=$1
+    localfile=/tmp/$username.worktime.$(date +%Y%m%d)
+    remotefile=/public_html/worktime/$username.worktime.$(date +%Y%m%d)
+
+    perl scripts/getWorkTime.pl -y $(date +%Y) -t ods -u $username -f $localfile
+
+    sftp katkola <<EOF
+    put $localfile.ods $remotefile.ods
+EOF
+
+}
+
+putti kivilahtio
+putti jraisa
+putti janPasi
+putti taskula
+putti sundahl
+putti JukkaAaltonen
+
